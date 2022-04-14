5565b2a7f851a8597a2f3c0031c27bba555b3704
LeeTomahawk <bartek20122013@gmail.com>
2022-04-14 14:22:58 +0200

    Koniec

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import "./App.css";
 import axios from "axios";
-import Image from "react-bootstrap/Image";
-import { Accordion } from "react-bootstrap";
 import Pokemons from "./components/pokemons";
 import { PokeDetails } from "./components/intefaces";
+import { Col, Row } from "react-bootstrap";
 
 function App() {
   const [pokemons, setPokemons] = useState<PokeDetails[]>([]);
@@ -53,10 +50,45 @@ function App() {
     });
     setPokemons(sortArray);
   }
+  function sortNameDESC() {
+    let sortArray = [...pokemons].sort((a, b) => {
+      return a.name > b.name ? -1 : 1;
+    });
+    setPokemons(sortArray);
+  }
+  function sortTypeASC() {
+    let sortArray = [...pokemons].sort((a, b) => {
+      return a.types[0].type.name > b.types[0].type.name ? 1 : -1;
+    });
+    setPokemons(sortArray);
+  }
+  function sortTypeDESC() {
+    let sortArray = [...pokemons].sort((a, b) => {
+      return a.types[0].type.name > b.types[0].type.name ? -1 : 1;
+    });
+    setPokemons(sortArray);
+  }
   return (
     <Container>
-      <Button onClick={() => changeLimit()}>ADD</Button>
-      <Button onClick={() => sortNameASC()}>sort</Button>
+      <Row className="justify-content-md-center">
+        <Col className="text-center">
+          <Button className="m-2 p-2" onClick={() => changeLimit()}>
+            Add more pokemons
+          </Button>
+          <Button className="m-2 p-2" onClick={() => sortNameASC()}>
+            Sort by name asc
+          </Button>
+          <Button className="m-2 p-2" onClick={() => sortNameDESC()}>
+            Sort by name desc
+          </Button>
+          <Button className="m-2 p-2" onClick={() => sortTypeASC()}>
+            Sort by first type asc
+          </Button>
+          <Button className="m-2 p-2" onClick={() => sortTypeDESC()}>
+            Sort by first type desc
+          </Button>
+        </Col>
+      </Row>
       <Pokemons pokemonsData={pokemons} />
     </Container>
   );
